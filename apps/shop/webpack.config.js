c64033231aabc5efe4da9ea9ed734778b34246e4
Scott Humphries <git@sscotth.io>
2022-08-02 10:28:17 -0500

    LongLiveCHIEF proposal

--- apps/shop/webpack.config.js
+++ apps/shop/webpack.config.js
@@ -1,6 +1,18 @@
 const withModuleFederation = require('@nrwl/react/module-federation');
 const moduleFederationConfig = require('./module-federation.config');
+const { merge } = require('webpack-merge');
 
-module.exports = withModuleFederation({
-  ...moduleFederationConfig,
-});
+const federatedModules = async () =>
+  withModuleFederation({ ...moduleFederationConfig });
+
+module.exports = async (config, context) => {
+  const federatedModuleConfig = await federatedModules();
+  return merge(federatedModuleConfig(config), {
+    module: {
+      //...module configs
+    },
+    resolve: {
+      //... resolve configs
+    },
+  });
+};
