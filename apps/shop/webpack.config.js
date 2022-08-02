7e36b8e837dc04fe040150d9c73bf02e50dc4439
Scott Humphries <git@sscotth.io>
2022-08-02 11:26:34 -0500

    with NodePolyfillPlugin

--- apps/shop/webpack.config.js
+++ apps/shop/webpack.config.js
@@ -1,26 +1,16 @@
 const withModuleFederation = require('@nrwl/react/module-federation');
 const moduleFederationConfig = require('./module-federation.config');
+const { merge } = require('webpack-merge');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
 const federatedWebpack = withModuleFederation({
   ...moduleFederationConfig,
 });
 
-module.exports = new Promise((resolve) => {
+module.exports = new Promise((resolve) =>
   federatedWebpack.then((fn) => {
-    resolve((config) => {
-      config.module.rules.push({
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'dts-loader',
-            options: {
-              ...moduleFederationConfig,
-            },
-          },
-        ],
-      });
-      return fn(config);
-    });
-  });
-});
+    resolve((config) =>
+      fn(merge(config, { plugins: [new NodePolyfillPlugin()] }))
+    );
+  })
+);
