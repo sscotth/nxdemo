03280a34535381337d8be26d93125ae7ee68011e
Scott Humphries <git@sscotth.io>
2022-08-02 11:20:36 -0500

    codrin-iftimie example

--- apps/shop/webpack.config.js
+++ apps/shop/webpack.config.js
@@ -1,6 +1,26 @@
 const withModuleFederation = require('@nrwl/react/module-federation');
 const moduleFederationConfig = require('./module-federation.config');
 
-module.exports = withModuleFederation({
+const federatedWebpack = withModuleFederation({
   ...moduleFederationConfig,
+});
+
+module.exports = new Promise((resolve) => {
+  federatedWebpack.then((fn) => {
+    resolve((config) => {
+      config.module.rules.push({
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'dts-loader',
+            options: {
+              ...moduleFederationConfig,
+            },
+          },
+        ],
+      });
+      return fn(config);
+    });
+  });
 });
